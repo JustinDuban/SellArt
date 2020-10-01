1087bf3f8bd119ef6db3abf406710678257fb648
JustinDuban <justin.duban@free.fr>
2020-10-01 09:14:37 +0200

    added navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/AboutUs';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+import logo from './logo.svg';
 import axios from 'axios'
 
 class App extends Component {
@@ -17,8 +24,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from the frontend!</h1>
-        <h1>{this.state.response.body}</h1>
+        
+        <BrowserRouter>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              SellArt, a new sight on all arts!
+            </p>
+            <Navigation />
+          </header>
+          <h1>Hello from the frontend!</h1>
+          <h1>{this.state.response.body}</h1>
+          <div>
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/about" component={About}/>
+             <Route path="/contact" component={Contact}/>
+             <Route component={Error}/>
+           </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
